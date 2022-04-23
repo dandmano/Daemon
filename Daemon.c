e9be0933bbba85184f5948b889161881963368db
dandman <demoon.pps@gmail.com>
2022-04-23 16:23:32 +0200

    Base project changes - ready to code

--- Daemon.c
+++ Daemon.c
@@ -1,9 +1,5 @@
-﻿// Daemon.cpp : Defines the entry point for the application.
-//
-
-#include "Daemon.h"
+﻿#include "Headers.h"
 
 int main(int argc, char* argv[]) {
-	printf("elo");
-	return 0;
+	
 }
---
+++ Headers.h
@@ -0,0 +1,18 @@
+﻿//Headers - plik zawiera wszystkie niezbędne do działania programu nagłówki
+
+#pragma once
+
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <syslog.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
+#include <utime.h>
